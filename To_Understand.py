fe00022287d18732ccb44e6781021e61c2a64172
Stefano <stefanocarraro00@gmail.com>
2023-12-29 18:18:40 +0100

    update 29/12

--- To_Understand.py
+++ To_Understand.py
@@ -9,7 +9,9 @@ import numpy as np
 #This is a custom function that writes all the decision variables to an excel file.
 #from Helper_Functions.exportDecisionVariables import exportDecisionVariables
 
-SystemDemand= pandas.read_excel(EconomicDispatch/Assignment3_ValidationSystem, sheet_name = 'SystemDemand', index_col= 0)
+filename = 'Assignment3_ValidationSystem.xlsx'
+
+SystemDemand= pandas.read_excel(filename, sheet_name = 'SystemDemand', index_col= 0)
 
 model.T = Set(ordered = True, initialize = SystemDemand.index) #Set for time steps 
 print(model.T)	
